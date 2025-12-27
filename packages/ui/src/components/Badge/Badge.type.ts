a0262173bf84662da579f8c1783c9d43978fcf8c
xamuel98 <samuelakintomiwa98@gmail.com>
2025-12-27 01:38:54 +0100

    feat(badge): implement Badge component and CircleDashedIcon

---
+++ packages/ui/src/components/Badge/Badge.type.ts
@@ -0,0 +1,59 @@
+import type { VariantProps } from "class-variance-authority";
+import type { badgeVariants } from "./Badge.variants.js";
+import type { ReactNode } from "react";
+
+export type BadgeTone =
+  | "default"
+  | "info"
+  | "success"
+  | "attention"
+  | "warning"
+  | "critical";
+
+export type BadgeProgress = "incomplete" | "partiallyComplete" | "complete";
+
+interface BaseBadgeProps
+  extends React.HTMLAttributes<HTMLSpanElement>,
+    VariantProps<typeof badgeVariants> {
+  /**
+   * The content to display inside the badge.
+   */
+  children?: ReactNode;
+  /**
+   * Colors and labels the badge with the given tone.
+   * @default 'default'
+   */
+  tone?: BadgeTone;
+  /**
+   * Defaults to 'medium'.
+   */
+  size?: "medium" | "small";
+  /**
+   * Pass a custom accessibilityLabel.
+   */
+  toneAndProgressLabelOverride?: string;
+}
+
+interface ProgressBadgeProps extends BaseBadgeProps {
+  /**
+   * Render a pip showing the progress of a given task.
+   */
+  progress?: BadgeProgress;
+  /**
+   * Icon is not allowed when progress is set.
+   */
+  icon?: undefined;
+}
+
+interface IconBadgeProps extends BaseBadgeProps {
+  /**
+   * Icon to display to the left of the badge’s content.
+   */
+  icon?: React.ReactNode;
+  /**
+   * Progress is not allowed when icon is set.
+   */
+  progress?: undefined;
+}
+
+export type BadgeProps = ProgressBadgeProps | IconBadgeProps;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,4 @@ export * from "./icons/index.js";
 export { Button } from "./components/Button/index.js";
 export { Spinner } from "./components/Spinner/index.js";
 export { ButtonGroup } from "./components/ButtonGroup/index.js";
+export { Badge } from "./components/Badge/index.js";
