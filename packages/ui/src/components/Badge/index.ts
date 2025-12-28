00d0a5394f2a32f825e217f0d9b5399257fa970d
xamuel98 <samuelakintomiwa98@gmail.com>
2025-12-29 00:55:48 +0100

    refactor: standardize component exports to include types and variants

--- packages/ui/src/components/Badge/index.ts
+++ packages/ui/src/components/Badge/index.ts
@@ -1,1 +1,3 @@
 export { Badge } from "./Badge.js";
+export { badgeVariants } from "./Badge.variants.js";
+export type { BadgeProps, BadgeTone } from "./Badge.type.js";
--- packages/ui/src/components/Button/index.ts
+++ packages/ui/src/components/Button/index.ts
@@ -1,1 +1,3 @@
 export { Button } from "./Button.js";
+export { buttonVariants } from "./Button.variants.js";
+export type { ButtonProps } from "./Button.type.js";
